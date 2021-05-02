d995cd7cd2bf22810a0c2f6e1955e1cd8503eb1d
Derrmos <denoyer.william@gmail.com>
2021-05-02 19:48:25 +0200

    Adding sitemap

---
+++ sitemap.php
@@ -0,0 +1,17 @@
+<?php
+    require './include/header.inc.php';
+?>
+    <main>
+        <h1>Plan du site</h1>
+        <ul id="sitemap_list">
+            <li><a href="./">Page d'accueil</a></li>
+            <li><a href="./search.php">&gt; Page de recherche</a></li>
+            <li><a href="./movies.php">&gt; Films</a></li>
+            <li><a href="./series.php">&gt; Séries</a></li>
+            <li><a href="./statistics.php">&gt; Statistiques</a></li>
+            <li><a href="./sitemap.php">&gt; Plan du site</a></li>
+        </ul>
+    </main>
+<?php
+    require './include/footer.inc.php';
+?>
